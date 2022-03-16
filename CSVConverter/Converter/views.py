caa9a43e3f95b380060b5b7562bd9af17734f934
Setho Ehrmann <ehrmannsetho@gmail.com>
2022-03-16 16:25:06 +0100

    adding file upload

--- CSVConverter/Converter/views.py
+++ CSVConverter/Converter/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views import View
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+class UploadFileView(View):
+    def get(self, request):
+        return render(request, "converter/converter.html")
+    def post(self, request):
+        print(request.FILES["csv"])
+        return HttpResponseRedirect("csv-to-xlsx")
+
 
 def index(request):
     return render(request, "converter/index.html")
-
-def converter(request):
-    return render(request, "converter/converter.html")
