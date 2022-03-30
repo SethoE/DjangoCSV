79a180db5a606c0ad608d59848859c99d62b96c2
Setho Ehrmann <ehrmannsetho@gmail.com>
2022-03-30 09:33:31 +0200

    pyc changes

--- CSVConverter/CSVConverter/settings.py
+++ CSVConverter/CSVConverter/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static/"
 ] # For adding folders
+
 MEDIA_ROOT = BASE_DIR / "uploads"
 
 # Default primary key field type
